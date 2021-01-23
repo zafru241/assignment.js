69e1ca95d9e1a9521dd39523ec23ea4ea19600ff
zafru241 <syedzafrullah99@gmail.com>
2021-01-23 19:50:38 +0600

    coding assignment.js

--- assignment.js
+++ assignment.js
@@ -1,26 +1,28 @@
-// function kilometerToMeter(kilometer){
-//     let meter = kilometer* 1000;
-//     return meter;
-// }
-// let result = kilometerToMeter(10);
-// console.log(result);
+// https://github.com/zafru241/assignment.js/blob/main/assignment.js
+
+function kilometerToMeter(kilometer){
+    let meter = kilometer* 1000;
+    return meter;
+}
+let result = kilometerToMeter(10);
+console.log(result);
 
 
-// let watchPrice = 50;
-// let mobilePrice = 100;
-// let laptopPrice = 500;
+let watchPrice = 50;
+let mobilePrice = 100;
+let laptopPrice = 500;
 
-// function budgetCalculator(watch, mobile, laptop){
-//     let watchPrice = watch * 50;
-//     let mobilePrice = mobile * 100;
-//     let laptopPrice = laptop * 500;
-//     return watchPrice + mobilePrice + laptopPrice;
-// }
+function budgetCalculator(watch, mobile, laptop){
+    let watchPrice = watch * 50;
+    let mobilePrice = mobile * 100;
+    let laptopPrice = laptop * 500;
+    return watchPrice + mobilePrice + laptopPrice;
+}
    
-// let totalPrice = budgetCalculator(10, 10, 10);
-// console.log(totalPrice);
+let totalPrice = budgetCalculator(10, 10, 10);
+console.log(totalPrice);
 
-let  = 
+
 
 
 function hotelCost(rent){
@@ -29,25 +31,20 @@ function hotelCost(rent){
     let rent2 = totalRent-50;
 
     if(rent<= 10){
-        building = floor * 15 * brickPerFeet;
+        totalRent = rent * 100 ;
        
         }
-        else if(floor<=20){
-            let floor1 = 10 * 15 *brickPerFeet;
-            let remainingFloor = floor-10;
-            let floor2 = remainingFloor *12 * brickPerFeet;
-            building = floor1 + floor2;
+        else if(rent<= 11){
+            let rent1 = 11 * 80 ;
+            totalRent = totalRent + rent1;
         }
-        else{
-            let floor1 =  10 * 15 *brickPerFeet;
-            let floor2 = 10 * 12 * brickPerFeet;
-            let remainingFloor = floor-20;
-            let floor3 = remainingFloor * 10 * brickPerFeet;
-            building = floor1 + floor2 + floor3;
+        else if(rent<= 21){
+            let rent2 = 21 * 50 ;
+            totalRent = totalRent + rent2 + rent2;
         }   
-        return building;  
+        return totalRent;  
 }
-let result = brickCalculator(22);
+let result = hotelCost(rent);
 console.log(result);
 
 
